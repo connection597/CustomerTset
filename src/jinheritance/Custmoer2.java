9ab3e1b6ac3df7558b3b1382a53e68cdc09cfe33
connection579 <wlswn4118@naver.com>
2020-11-17 19:33:46 +0900

    상속 클래스를 이용한 고객 관리 시스템 연습

---
+++ src/jinheritance/Custmoer2.java
@@ -0,0 +1,28 @@
+package jinheritance;
+
+public class Custmoer2 {
+	protected int customerID;
+	protected String customerName;
+	protected String customerGrade;
+	int bonusPoint;
+	double bonusRatio;
+    
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID=customerID;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName=customerName ;
+	}
+	public String getCustomerGrade() {
+		return customerGrade;
+	}
+	public void setCustomerGrade(String customerGrade) {
+		this.customerGrade =customerGrade;
+	}
+}
---
+++ src/jinheritance/Customer.java
@@ -0,0 +1,57 @@
+package jinheritance; //customer클래스 구현하기
+
+public class Customer {  //변수
+	protected int customerID;
+	protected String customerName;
+	protected String customerGrade;
+	int bonusPoint;
+	double bonusRatio;
+	
+	public Customer() { // 기본 등급과 적립비율
+		customerGrade ="SILVER";
+		bonusRatio = 0.01;
+	}
+	public void calcPrice(int price) {
+		bonusPoint +=price*bonusPoint; //보너스포인트 계산
+	}
+	
+	public String showCustomerInfo() { // 고객정보 반환하기 메소드
+		return customerName +"님의 등급은"
+	+ customerGrade +"이며 , 보너스포인트는"+bonusPoint+"입니다";
+	}
+	public class VIPCustomer{
+		private int customerID;
+		private String customerName;
+		private String custemerGrade;
+		int bonusPoint;
+		double bonusRatio;
+		
+		private int agentID; //VIP고객 상담원 아이디
+		double saleRatio; //할인율
+		
+		public VIPCustomer() {
+			customerGrade = "VIP"; //고객등급
+			bonusRatio =0.05; //보너스 적립
+			saleRatio =0.1;// 할인율 10%
+		}
+		public int calcPrice(int price) {
+			bonusPoint +=price*bonusRatio;
+			return price -(int)(price*saleRatio);//할인률 적용
+		}
+		public String showCustomerInfo() {
+			return customerName +"님의 등급은"
+		+customerGrade+"이며, 보너스포인트는"+bonusPoint+"입니다";
+		}
+		
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName=customerName;
+		}
+	public void setCustomerID(int customerID) {
+		this.customerID= customerID;
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
